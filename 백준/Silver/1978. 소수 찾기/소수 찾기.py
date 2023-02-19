91db5c0d3b9fa3362d8ffde0c6739cb4d424f3ba
Seohyun <81468092+Seohyun-0206@users.noreply.github.com>
2023-02-19 17:59:04 +0900

    [Silver V] Title: 소수 찾기, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+x = list(map(int, input().split()))
+cnt = 0
+for i in x:
+
+    flag = 0
+    if i > 1:
+        for j in range(2, i - 1):
+            if i % j == 0:
+                flag = 1
+
+        if flag == 0:
+            cnt += 1
+
+print(cnt)
