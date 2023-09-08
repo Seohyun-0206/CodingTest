31e572ffd5d85b70d771db6e6fe478cd75fc5052
Seohyun <tjgus5935@gmail.com>
2023-09-08 20:26:37 +0900

    [Silver I] Title: 카드 구매하기, Time: 216 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
@@ -0,0 +1,14 @@
+import sys
+n = int(input())
+
+
+p = [0] + (list(map(int, sys.stdin.readline().split())))
+
+d = [0] * 1001
+
+for i in range(1, n + 1):
+    d[i] = p[i]
+    for j in range(1, i):
+        d[i] = max(d[i - j] + p[j], d[i])
+
+print(d[n])
