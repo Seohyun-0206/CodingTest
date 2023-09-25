3104a79d9f0f26ac7b4d8b8a9c1a7218e7e63311
Seohyun <tjgus5935@gmail.com>
2023-09-25 21:06:16 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -1,5 +1,5 @@
 def solution(num_list):
-    even = [i for i in num_list if i % 2 == 0]
-    length = len(even)
-    answer = [length, len(num_list) - length]
+    answer = [0, 0]
+    for n in num_list:
+        answer[n % 2] += 1
     return answer
