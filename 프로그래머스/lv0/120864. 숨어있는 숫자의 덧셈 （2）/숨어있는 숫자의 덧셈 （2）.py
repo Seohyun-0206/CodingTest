2683b6bc212dc63d7dfc9c12798c0b2ab27eee41
Seohyun <tjgus5935@gmail.com>
2023-10-02 16:08:00 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
+++ 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
@@ -1,13 +1,3 @@
 def solution(my_string):
-    answer = 0
-    sum = 0
-    for i in my_string:
-        if i.isdigit():
-            sum *= 10
-            sum += int(i)
-        else:
-            answer += sum
-            sum = 0
-    if my_string[-1].isdigit():
-        answer += sum
-    return answer
+    answer = ''.join(i if i.isdigit() else ' ' for i in my_string)
+    return sum(int(i) for i in answer.split())
