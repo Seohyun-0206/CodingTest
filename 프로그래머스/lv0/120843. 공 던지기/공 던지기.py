f4779ab74dabae547ad70d7d6150f97c4b9ee469
Seohyun <tjgus5935@gmail.com>
2023-10-05 23:07:07 +0900

    [level 0] Title: 공 던지기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120843. 공 던지기/공 던지기.py
+++ 프로그래머스/lv0/120843. 공 던지기/공 던지기.py
@@ -1,8 +1,3 @@
 def solution(numbers, k):
-    answer = 1
-    for i in range(0, k-1):
-        answer += 2
-        if answer > len(numbers):
-            answer %= len(numbers)
         
-    return answer
+    return numbers[2 * (k - 1) % len(numbers)]
