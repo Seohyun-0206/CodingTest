178ae45029d6169cde4906cce9fe19877629a912
Seohyun <tjgus5935@gmail.com>
2023-10-06 14:40:47 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.py
@@ -0,0 +1,10 @@
+import math
+def solution(a, b):
+    gcd = math.gcd(a, b)
+    a //= gcd
+    b //= gcd
+    for i in (2, 5):
+        while b % i == 0:
+            b //= i
+    
+    return 1 if b == 1 else 2
