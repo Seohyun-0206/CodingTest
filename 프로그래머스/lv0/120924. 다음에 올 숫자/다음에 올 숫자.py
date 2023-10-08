a270825f4eda08d53d79c280deb8b75608973113
Seohyun <tjgus5935@gmail.com>
2023-10-08 21:11:05 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.py
@@ -0,0 +1,9 @@
+def solution(common):
+    if common[1] - common[0] == common[2] - common[1]:
+        d = common[1] - common[0]
+        answer = common[len(common) - 1] + d
+    elif common[1] // common[0] == common[2] // common[1]:
+        r = common[1] // common[0]
+        answer = common[len(common) - 1] * r
+        
+    return answer 
