f060d4a5e28b2770fb35e62874ca193db3ea9ac5
Seohyun <tjgus5935@gmail.com>
2023-10-08 21:54:41 +0900

    [level 0] Title: 특이한 정렬, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.py
+++ 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.py
@@ -1,6 +1,3 @@
 def solution(numlist, n):
-    rank = {}
-    for i in numlist:
-        rank[i] = abs(n - i)
-    r = dict(sorted(rank.items(), key=lambda x:(x[1], -x[0])))
-    return list(r.keys())
+    answer = sorted(numlist, key=lambda x:(abs(n - x), -x))
+    return answer
