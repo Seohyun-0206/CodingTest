d6e4209a47a62eedaadebe19b06232840fdb7983
Seohyun <tjgus5935@gmail.com>
2023-10-13 03:54:20 +0900

    [level 1] Title: 모의고사, Time: 3.51 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,46 +1,22 @@
 def solution(answers):
-    correct = {1 : 0, 2 : 0, 3 : 0}
+    pattern1 = [1, 2, 3, 4, 5]
+    pattern2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    pattern3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
     
-    n1 = 1
-    n2 = 5
-    n3 = 3
-    for i in range(len(answers)):
-        if n1 == answers[i]:
-            correct[1] += 1
-        if i % 2 == 0:
-            n2 = number2(n2)
-            if answers[i] == 2:
-                correct[2] += 1
-        elif n2 == answers[i]:
-            correct[2] += 1
-        if n3 == answers[i]:
-            correct[3] += 1    
-        
-        n1 += 1
-        if n1 > 5:
-            n1 = 1
-        if i % 2 != 0:
-            n3 = number3(n3)
-            
-    correct = sorted(correct.items(), key=lambda x:x[1], reverse=True)
+    score = [0] * 3
+    for i, ans in enumerate(answers):
+        if ans == pattern1[i % len(pattern1)]:
+            score[0] += 1
+        if ans == pattern2[i % len(pattern2)]:
+            score[1] += 1
+        if ans == pattern3[i % len(pattern3)]:
+            score[2] += 1
+    
     answer = []
-    for i in range(3):
-        if correct[i][1] == correct[0][1]:
-            answer.append(correct[i][0])
+    max_s = max(score)
+    for i, s in enumerate(score):
+        if s == max_s:
+            answer.append(i + 1)
     return answer
 
     
-def number2(n2):
-    if n2 == 1: n2 = 3
-    elif n2 == 5: n2 = 1
-    else: n2 += 1
-    
-    return n2
-
-def number3(n3):
-    if n3 == 3: n3 = 1
-    elif n3 == 1 or n3 == 4: n3 += 1
-    elif n3 == 2: n3 = 4
-    elif n3 == 5: n3 = 3
-    
-    return n3
