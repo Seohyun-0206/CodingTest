0c9373d69119c85d69acfc9a1565a055908fce47
Seohyun <tjgus5935@gmail.com>
2023-10-14 23:41:52 +0900

    [Silver I] Title: 카드 구매하기 2, Time: 212 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/16194. 카드 구매하기 2/카드 구매하기 2.py
+++ 백준/Silver/16194. 카드 구매하기 2/카드 구매하기 2.py
@@ -1,11 +1,8 @@
 import sys
 n = int(input())
+p = [0] + list(map(int, sys.stdin.readline().split()))
 
-
-p = [0] + (list(map(int, sys.stdin.readline().split())))
-
-d = [0] * 1001
-
+d = [10001] * 1001
 for i in range(1, n + 1):
     d[i] = p[i]
     for j in range(1, i):
