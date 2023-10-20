b9e115151df6473ad2c50bee6dfc9f6bc2574481
Seohyun <tjgus5935@gmail.com>
2023-10-21 02:35:48 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -1,7 +1,7 @@
 def solution(n):
     answer = n + 1
     while True:
-        if str(bin(n)[2:]).count('1') == str(bin(answer)[2:]).count('1'):
+        if bin(n).count('1') == bin(answer).count('1'):
             break
         answer += 1
     return answer
